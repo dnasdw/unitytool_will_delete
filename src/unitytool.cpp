47fc8d16b353a64d7b14fba633c97877d2be893a
Sun Daowen <sundaowen@gmail.com>
2018-06-12 14:35:10 +0800

    init unitytool

---
+++ src/unitytool.cpp
@@ -0,0 +1,6 @@
+#include "unitytool.h"
+
+int UMain(int argc, UChar* argv[])
+{
+	return 0;
+}
---
+++ src/unitytool.h
@@ -0,0 +1,6 @@
+#ifndef UNITYTOOL_H_
+#define UNITYTOOL_H_
+
+#include <sdw.h>
+
+#endif	// UNITYTOOL_H_
