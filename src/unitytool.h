5c4f1d9deb5f1c40d6dd0e840bb5f8e3c652df57
Sun Daowen <sundaowen@gmail.com>
2018-06-12 15:26:41 +0800

    copy from 3dstool

--- src/unitytool.h
+++ src/unitytool.h
@@ -3,4 +3,53 @@
 
 #include <sdw.h>
 
+class CUnityTool
+{
+public:
+	enum EParseOptionReturn
+	{
+		kParseOptionReturnSuccess,
+		kParseOptionReturnIllegalOption,
+		kParseOptionReturnNoArgument,
+		kParseOptionReturnUnknownArgument,
+		kParseOptionReturnOptionConflict
+	};
+	enum EAction
+	{
+		kActionNone,
+		kActionExtract,
+		kActionCreate,
+		kActionHelp
+	};
+	enum EFileType
+	{
+		kFileTypeUnknown,
+		kFileTypeAssetBundle,
+		kFileTypeAssets
+	};
+	struct SOption
+	{
+		const UChar* Name;
+		int Key;
+		const UChar* Doc;
+	};
+	CUnityTool();
+	~CUnityTool();
+	int ParseOptions(int a_nArgc, UChar* a_pArgv[]);
+	int CheckOptions();
+	int Help();
+	int Action();
+	static SOption s_Option[];
+private:
+	EParseOptionReturn parseOptions(const UChar* a_pName, int& a_nIndex, int a_nArgc, UChar* a_pArgv[]);
+	EParseOptionReturn parseOptions(int a_nKey, int& a_nIndex, int a_nArgc, UChar* a_pArgv[]);
+	bool extractFile();
+	bool createFile();
+	EAction m_eAction;
+	EFileType m_eFileType;
+	UString m_sFileName;
+	bool m_bVerbose;
+	UString m_sMessage;
+};
+
 #endif	// UNITYTOOL_H_
